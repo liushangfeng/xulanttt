4034fe0122d07575b7108eeb17b5b932df6ff097
lanfeijianyuan <1506430203@qq.com>
2017-06-03 17:20:23 +0800

    commit

--- src/main/java/org/coach/tdd/template/Library.java
+++ src/main/java/org/coach/tdd/template/Library.java
@@ -1,7 +1,106 @@
 package org.coach.tdd.template;
 
 public class Library {
+    private int countAlive;
+    private final int HEIGHT=15;//游戏界面size
+    private final int WIDTH =60;
+    private char[][] cell = new char[HEIGHT][WIDTH];
+    private final char Y ='*'; //用字符*代表一个细胞
+    private final char No = ' ';
+
+    public boolean haveThreeAlive(int adrX,int adrY, int index) {
+        int flag = 0;
+        countAlive = index;
+        if (countAlive == 3) {
+            cell[adrX][adrY] = Y;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public boolean haveTwokeep(int adrX,int adrY,int index) {
+        //int flag = 0;
+        // countAlive=index;
+        // if(countAlive == 2)
+        return true;
+
+    }
+
+    public boolean haveDead(int adrX,int adrY) {
+        cell[adrX][adrY] = No;
+            return true;
+
+    }
+    Library(){
+        cell[3][4] = cell[3][5] = cell[3][6] =Y;
+        //glider
+        cell[10][4] =  cell[10][5] =  cell[10][6] =  cell[11][6] =  cell[12][5] = Y;
+        System.out.print("initial cell:");
+        for(int x=0;x<HEIGHT;x++){
+            for(int y=0;y<WIDTH;y++)
+                System.out.print(cell[x][y]);
+            System.out.print("\n");}
+        System.out.print("\n");
+        System.out.print("\n");
+
+    }
+    private int cellState(int y,int x) {
+        boolean isEmpty = (y < 0 || y >= HEIGHT || x < 0 || x >= WIDTH || cell[y][x] != Y ) ;
+        return isEmpty ? 0 : 1 ;
+    }
+    private int getNeighbors(int y,int x) {
+        int n = 0; //neighbor
+        n += cellState(y - 1, x - 1);
+        n += cellState(y - 1, x);
+        n += cellState(y - 1, x + 1);
+        n += cellState(y, x - 1);
+        n += cellState(y, x + 1);
+        n += cellState(y + 1, x - 1);
+        n += cellState(y + 1, x);
+        n += cellState(y + 1, x + 1);
+        System.out.print(n);
+        return n-1;
+    }
+
+    public boolean resolve(int i,int j) {
+        int a,b;
+        a=i;
+        b=j;
+        boolean flag;
+       int  n = getNeighbors(a,b);
+        System.out.print(n);
+        if(n == 3)
+           flag= haveThreeAlive(a,b,n);
+        else if(n == 2)
+             flag = haveTwokeep(a,b,n);
+        else
+            flag=haveDead(a,b);
+        for(int x=0;x<HEIGHT;x++){
+            for(int y=0;y<WIDTH;y++)
+                System.out.print(cell[x][y]);
+                     System.out.print("\n");}
+        return flag;
+
+    }
+
     public boolean someLibraryMethod() {
         return true;
     }
+
+    public void main(){
+        int checkY,checkX;
+        Library selfCell = new Library();
+        //for(int i=0;i<HEIGHT;i++)
+            //for(int j=0;j<WIDTH;j++) {
+                 checkY = 4;
+                checkX = 3;
+
+               // selfCell.cellState(checkX, checkY);
+               // getNeighbors(checkX, checkY);
+                resolve(checkX, checkY);
+
+    }
+
 }
+
--- src/test/java/org/coach/tdd/template/LibraryTest.java
+++ src/test/java/org/coach/tdd/template/LibraryTest.java
@@ -2,6 +2,7 @@ package org.coach.tdd.template;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LibraryTest {
@@ -10,4 +11,31 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    @Test
+    public void cellRealizeTestThreeAlive(){
+        Library cell =  new Library();
+        assertTrue(cell.haveThreeAlive(1,3,3));
+    }
+
+    @Test
+    public void cellRealizeTestTwoKeep() throws Exception {
+        Library cell =  new Library();
+        assertTrue(cell.haveTwokeep(1,3,2));
+    }
+
+    @Test
+    public void cellRealizeTestDead() throws Exception {
+        Library cell =  new Library();
+        assertTrue(cell.haveDead(1,3));
+    }
+
+    @Test
+    public void demon() throws Exception {
+        Library cell =  new Library();
+        assertTrue(cell.resolve(4,5));
+    }
+
+
 }
+
+
